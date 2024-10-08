3769e214ad7ef5f2fae3146b702eee1724fe549c
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-08 14:41:05 +0200

    first function in Navigation Bar

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -1,47 +1,69 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.green[900],
-          title: Text("Profile", style: TextStyle(color: Colors.white))),
-      body: Center(
-          child: Column(children: [
-        Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: CircleAvatar(
-              radius: 100,
-              backgroundImage: AssetImage("assets/images/bike.jpeg")),
-        ),
-        SizedBox(height: 25),
-        Text(
-          "Kaya Müller",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            "Fotograf",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-            padding: const EdgeInsets.all(20.0),
-            // hier wird später die Variable aus der Liste eingesetzt
-            child: Text(
-              """Lorem eu quis aute magna irure velit officia sunt reprehenderit irure do qui consectetur eiusmod. 
+      home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Colors.green[900],
+              title: Text("Profile", style: TextStyle(color: Colors.white))),
+          body: Center(
+              child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage("assets/images/bike.jpeg")),
+            ),
+            SizedBox(height: 25),
+            Text(
+              "Kaya Müller",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Fotograf",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(20.0),
+                // hier wird später die Variable aus der Liste eingesetzt
+                child: Text(
+                  """Lorem eu quis aute magna irure velit officia sunt reprehenderit irure do qui consectetur eiusmod. 
                       Nostrud officia anim elit veniam cupidatat duis quis cupidatat deserunt duis ullamco commodo ullamco. 
                       Reprehenderit adipisicing reprehenderit aliqua sint. Et id laboris amet anim tempor culpa velit occaecat. 
                       Nostrud duis tempor dolor quis voluptate labore occaecat velit anim ad minim culpa ut officia. Incididunt i
                       ncididunt sunt laborum commodo ea voluptate non ut cupidatat.""",
-              textAlign: TextAlign.justify,
-            )),
-      ])),
-    ));
+                  textAlign: TextAlign.justify,
+                )),
+          ])),
+          bottomNavigationBar: BottomNavigationBar(
+              onTap: (index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              currentIndex: currentIndex,
+              unselectedItemColor: Colors.grey,
+              selectedItemColor: Colors.green,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.abc_rounded), label: "links"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.ac_unit_outlined), label: "rechts"),
+              ])),
+    );
   }
 }
