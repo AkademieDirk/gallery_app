f02a7211c55a79bfcd2f7c3b270ec82b9a7d36c9
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-09 20:57:43 +0200

    author List edited

--- lib/features/galleryview/screens/home_screen.dart
+++ lib/features/galleryview/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_app/experiment.dart/puzzle_effect.dart';
-import 'package:gallery_app/features/galleryview/lists/author_data.dart';
 import 'package:gallery_app/features/galleryview/lists/gallery_data.dart';
 import 'package:gallery_app/features/galleryview/screens/details_screen.dart';
 import 'package:gallery_app/widgets/author_item.dart';
@@ -57,10 +56,22 @@ class _HomeScreenState extends State<HomeScreen> {
                     // backgroundImage:
                   ),
                   //   ),
-                  Text(
-                    textAlign: TextAlign.center,
-                    galleryData[index].imageTitle,
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          child: galleryData[index].icon,
+                        ),
+                      ),
+                      Text(
+                        textAlign: TextAlign.center,
+                        galleryData[index].imageTitle,
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
                 ],
               ),
--- lib/widgets/gallery_item.dart
+++ lib/widgets/gallery_item.dart
@@ -1,12 +1,17 @@
+import 'package:flutter/material.dart';
+
 class GalleryItem {
-  late final imageTitle;
+  late final String imageTitle;
   late final String imageDate;
   late final String imageDescription;
   late final String imagePath;
+  late final Icon icon;
 
-  GalleryItem(
-      {required this.imageTitle,
-      required this.imageDate,
-      required this.imageDescription,
-      required this.imagePath});
+  GalleryItem({
+    required this.imageTitle,
+    required this.imageDate,
+    required this.imageDescription,
+    required this.imagePath,
+    required this.icon,
+  });
 }
